3397803734276ae25a936344c59c9a7452641ce7
mariliamessias <marilia.messias@mercadolivre.com>
2021-01-08 22:34:52 -0300

    GET /orders

---
+++ backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
@@ -0,0 +1,27 @@
+package com.devsuperior.dsdeliver.services;
+
+import com.devsuperior.dsdeliver.dto.OrderDTO;
+import com.devsuperior.dsdeliver.dto.ProductDTO;
+import com.devsuperior.dsdeliver.entities.Order;
+import com.devsuperior.dsdeliver.entities.Product;
+import com.devsuperior.dsdeliver.repositories.OrderRepository;
+import com.devsuperior.dsdeliver.repositories.ProductRepository;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    @Transactional(readOnly = true)
+    public List<OrderDTO> findAll(){
+        List<Order> orders = orderRepository.findOrdersWithProducts();
+
+        return orders.stream().map( x -> new OrderDTO(x)).collect(Collectors.toList());
+    }
+}
